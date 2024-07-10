8b91b11eb9ed635bbc7fb3c51336822725f5fb87
randaaz <randaaziz972@gmail.com>
2024-07-10 02:16:10 -0700

    module

---
+++ 0x02-Session_authentication/models/base.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+""" Module for Base class and related functionalities.
+"""
+from datetime import datetime
+from typing import TypeVar, List, Iterable
+from os import path
+import json
+import uuid
+
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+DATA = {}
+
+
+class Base():
+    """ Base class for all models.
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initializes a new instance of Base class.
+        """
+        s_class = str(self.__class__.__name__)
+        if DATA.get(s_class) is None:
+            DATA[s_class] = {}
+
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        if kwargs.get('created_at') is not None:
+            self.created_at = datetime.strptime(kwargs.get('created_at'),
+                                                TIMESTAMP_FORMAT)
+        else:
+            self.created_at = datetime.utcnow()
+        if kwargs.get('updated_at') is not None:
+            self.updated_at = datetime.strptime(kwargs.get('updated_at'),
+                                                TIMESTAMP_FORMAT)
+        else:
+            self.updated_at = datetime.utcnow()
+
+    def __eq__(self, other: TypeVar('Base')) -> bool:
+        """ Checks equality based on id.
+        """
+        if type(self) != type(other):
+            return False
+        if not isinstance(self, Base):
+            return False
+        return (self.id == other.id)
+
+    def to_json(self, for_serialization: bool = False) -> dict:
+        """ Converts instance to JSON serializable dictionary.
+        """
+        result = {}
+        for key, value in self.__dict__.items():
+            if not for_serialization and key[0] == '_':
+                continue
+            if type(value) is datetime:
+                result[key] = value.strftime(TIMESTAMP_FORMAT)
+            else:
+                result[key] = value
+        return result
+
+    @classmethod
+    def load_from_file(cls):
+        """  Loads instances from a file.
+        """
+        s_class = cls.__name__
+        file_path = ".db_{}.json".format(s_class)
+        DATA[s_class] = {}
+        if not path.exists(file_path):
+            return
+
+        with open(file_path, 'r') as f:
+            objs_json = json.load(f)
+            for obj_id, obj_json in objs_json.items():
+                DATA[s_class][obj_id] = cls(**obj_json)
+
+    @classmethod
+    def save_to_file(cls):
+        """ Saves instances to a file.
+        """
+        s_class = cls.__name__
+        file_path = ".db_{}.json".format(s_class)
+        objs_json = {}
+        for obj_id, obj in DATA[s_class].items():
+            objs_json[obj_id] = obj.to_json(True)
+
+        with open(file_path, 'w') as f:
+            json.dump(objs_json, f)
+
+    def save(self):
+        """ Saves current instance to the storage.
+        """
+        s_class = self.__class__.__name__
+        self.updated_at = datetime.utcnow()
+        DATA[s_class][self.id] = self
+        self.__class__.save_to_file()
+
+    def remove(self):
+        """ Removes current instance from the storage.
+        """
+        s_class = self.__class__.__name__
+        if DATA[s_class].get(self.id) is not None:
+            del DATA[s_class][self.id]
+            self.__class__.save_to_file()
+
+    @classmethod
+    def count(cls) -> int:
+        """  Counts the number of instances.
+        """
+        s_class = cls.__name__
+        return len(DATA[s_class].keys())
+
+    @classmethod
+    def all(cls) -> Iterable[TypeVar('Base')]:
+        """ Returns all instances.
+        """
+        return cls.search()
+
+    @classmethod
+    def get(cls, id: str) -> TypeVar('Base'):
+        """ Gets an instance by id.
+        """
+        s_class = cls.__name__
+        return DATA[s_class].get(id)
+
+    @classmethod
+    def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
+        """ Searches for instances matching attributes.
+        """
+        s_class = cls.__name__
+
+        def _search(obj):
+            if len(attributes) == 0:
+                return True
+            for k, v in attributes.items():
+                if (getattr(obj, k) != v):
+                    return False
+            return True
+
+        return list(filter(_search, DATA[s_class].values()))
